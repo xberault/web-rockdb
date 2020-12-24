2b2e017b8504f39170e9f3260b1d415ae83dc054
Xavier Bérault <xavier.berault@gmail.com>
2020-12-24 16:21:45 +0100

    ajout connexion/inscription utilisateur + dépendences flask_login

---
+++ rockDB/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import (DataRequired,
+                                Email,
+                                EqualTo,
+                                Length,
+                                Optional)
+
+
+class SignupForm(FlaskForm):
+    """User Sign-up Form."""
+    name = StringField('Pseudo', validators=[DataRequired()])
+    password = PasswordField(
+        'Password')
+    confirm = PasswordField(
+        'Confirmez votre mot de passe'
+    )
+    submit = SubmitField('Confirmer')
+
+
+class LoginForm(FlaskForm):
+    """User Log-in Form."""
+    name = StringField('Pseudo', validators=[DataRequired()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
--- rockDB/views.py
+++ rockDB/views.py
@@ -1,9 +1,87 @@
 from .app import app
-from flask import render_template
+from .forms import LoginForm, SignupForm
+from .models import User
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_required, logout_user, current_user, login_user
+
 
 @app.route("/")
 def home():
     return render_template(
         "home.html",
-        title = "RockDB"
+        title="RockDB"
+    )
+
+
+@login_required
+@app.route("/dashboard")
+def dashboard():
+    return "allo"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    # Validate login attempt
+    if form.validate_on_submit():
+        user = User.user_from_username(form.name.data)
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            flash('Vous êtes maintenant connecté', 'success')
+            return redirect(next_page or url_for('home'))
+        flash('Erreur sur le pseudo/mot de passe', 'warning')
+        return redirect(url_for('login'))
+    return render_template(
+        'auth/login.html',
+        form=form,
+        title='Connexion',
+        template='login-page',
+        body="Connexion au compte utilisateur"
+    )
+
+
+from .commands import newuser
+from .models import load_user
+from flask import get_flashed_messages
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    get_flashed_messages()
+    form = SignupForm()
+    if form.validate_on_submit():
+        existing_user = load_user(form.name.data)
+        if existing_user:
+            flash('Ce pseudo est déjà pris', 'warning')
+            return redirect(url_for('signup'))
+        elif len(form.password.data) < 6:
+            flash('Votre mot de passe doit posséder au moins 6 charactères', 'warning')
+            return redirect(url_for('signup'))
+        elif form.password.data != form.confirm.data:
+            flash('Les mots de pases doivent être identiques', 'warning')
+            return redirect(url_for('signup'))
+        else:
+            u = User.register(form.name.data, form.password.data)
+            flash("Vous êtes maintenant membre de RockDB", "success")
+            return redirect(url_for('dashboard'))
+    return render_template(
+        'auth/signup.html',
+        title='Inscription',
+        form=form,
+        template='signup-page',
+        body="Inscription d'un utilisateur"
+    )
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    return render_template(
+        'auth/logout.html',
+        title='Logging out',
+        template='logout-page',
+        body="Deconnexion d'un utilisateur"
     )
